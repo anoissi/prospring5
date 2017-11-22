daf9b6d8c363c5e9b64f57b63829711294535ac1
anoissi <abdessattar.noi@gmail.com>
2017-11-23 00:07:13 +0100

    Dependency lookup - dependency

---
+++ chapter03/src/main/java/learn/prospring5/ch03/ioc/dl/pull/DependencyPull.java
@@ -0,0 +1,14 @@
+package learn.prospring5.ch03.ioc.dl.pull;
+
+import learn.prospring5.ch02.beans.dec.MessageRenderer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DependencyPull {
+    public static void main(String... args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext
+                ("spring/app-context.xml");
+        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
+        mr.render();
+    }
+}
