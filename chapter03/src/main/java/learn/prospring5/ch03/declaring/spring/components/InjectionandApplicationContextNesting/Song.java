14c86f19577b2b7ccd37f277644b06bfec6a5998
anoissi <abdessattar.noi@gmail.com>
2017-12-15 23:02:46 +0100

    nested application context

---
+++ chapter03/src/main/java/learn/prospring5/ch03/declaring/spring/components/InjectionandApplicationContextNesting/Song.java
@@ -0,0 +1,11 @@
+package learn.prospring5.ch03.declaring.spring.components.InjectionandApplicationContextNesting;
+
+public class Song {
+    private String title;
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getTitle() {
+        return title;
+    }
+}
