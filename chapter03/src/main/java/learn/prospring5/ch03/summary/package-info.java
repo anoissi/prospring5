95f16ef891474f90037546f26487aebce9d25c0c
anoissi <abdessattar.noi@gmail.com>
2017-12-17 14:41:46 +0100

    summary

---
+++ chapter03/src/main/java/learn/prospring5/ch03/summary/package-info.java
@@ -0,0 +1,22 @@
+package learn.prospring5.ch03.summary;
+
+/*
+In this chapter, we covered a lot of ground with both Spring Core and IoC in general. We showed you
+        examples of the types of IoC and presented the pros and cons of using each mechanism in your applications.
+        We looked at which IoC mechanisms Spring provides and when (and when not) to use each within your
+        applications. While exploring IoC, we introduced the Spring BeanFactory, which is the core component
+        for Spring’s IoC capabilities, and then ApplicationContext, which extends BeanFactory and provides
+        additional functionalities. For ApplicationContext, we focused on GenericXmlApplicationContext,
+        which allows external configuration of Spring by using XML. Another method to declare DI requirements
+        for ApplicationContext, that is, using Java annotations, was also discussed. Some examples with
+        AnnotationConfigApplicationContext and Java configuration were also included, just to slowly introduce
+        this way of configuring beans.
+        This chapter also introduced you to the basics of Spring’s IoC feature set including setter
+        injection, constructor injection, Method Injection, autowiring, and bean inheritance. In the
+        discussion of configuration, we demonstrated how you can configure your bean properties with a
+        wide variety of values, including other beans, using both XML and annotation type configurations and
+        GenericXmlApplicationContext.
+        This chapter only scratched the surface of Spring and Spring’s IoC container. In the next chapter,
+        you’ll look at some IoC-related features specific to Spring, and you’ll take a more detailed look at other
+        functionality available in Spring Core
+*/
