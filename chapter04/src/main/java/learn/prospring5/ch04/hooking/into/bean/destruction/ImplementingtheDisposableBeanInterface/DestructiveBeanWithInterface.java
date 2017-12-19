004034ef14d3ab6c8cb48bd9aa5943c3041e1452
anoissi <abdessattar.noi@gmail.com>
2017-12-19 10:50:09 +0100

    hooking into bean destruction : using destroy interface

---
+++ chapter04/src/main/java/learn/prospring5/ch04/hooking/into/bean/destruction/ImplementingtheDisposableBeanInterface/DestructiveBeanWithInterface.java
@@ -0,0 +1,44 @@
+package learn.prospring5.ch04.hooking.into.bean.destruction.ImplementingtheDisposableBeanInterface;
+
+import java.io.File;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.support.GenericXmlApplicationContext;
+public class DestructiveBeanWithInterface implements InitializingBean, DisposableBean {
+    private File file;
+    private String filePath;
+    @Override
+    public void afterPropertiesSet()  throws Exception {
+        System.out.println("Initializing  Bean");
+        if (filePath == null) {
+            throw new IllegalArgumentException(
+                    "You must  specify the filePath property of " +
+                            DestructiveBeanWithInterface.class);
+        }
+        this.file = new File(filePath);
+        this.file.createNewFile();
+        System.out.println("File exists: " +  file.exists());
+    }
+    @Override
+    public void destroy() {
+        System.out.println("Destroying  Bean");
+        if(!file.delete()) {
+            System.err.println("ERROR: failed  to delete file.");
+        }
+        System.out.println("File exists: " +  file.exists());
+    }
+    public void setFilePath(String filePath)  {
+        this.filePath =  filePath;
+    }
+    public static void main(String... args) throws Exception {
+        GenericXmlApplicationContext  ctx  =
+                new GenericXmlApplicationContext();
+        ctx.load("classpath:hookingIntoBeanDestruction/bean-destruction-inter.xml");
+        ctx.refresh();
+        DestructiveBeanWithInterface bean =
+                (DestructiveBeanWithInterface) ctx.getBean("destructiveBean");
+        System.out.println("Calling destroy()");
+        ctx.destroy();
+        System.out.println("Called destroy()");
+    }
+}
