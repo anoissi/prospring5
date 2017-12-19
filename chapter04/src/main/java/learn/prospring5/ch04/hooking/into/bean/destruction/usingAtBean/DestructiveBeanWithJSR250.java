cc97ddbdc8bfb97cda2c990c90bcc6879e6a5dd6
anoissi <abdessattar.noi@gmail.com>
2017-12-19 15:00:38 +0100

    bean destruction : @bean

---
+++ chapter04/src/main/java/learn/prospring5/ch04/hooking/into/bean/destruction/usingAtBean/DestructiveBeanWithJSR250.java
@@ -0,0 +1,32 @@
+package learn.prospring5.ch04.hooking.into.bean.destruction.usingAtBean;
+
+import java.io.File;
+public class DestructiveBeanWithJSR250 {
+    private File file;
+    private String filePath;
+
+    public void afterPropertiesSet()  throws Exception {
+        System.out.println("Initializing  Bean");
+        if (filePath == null) {
+            throw new IllegalArgumentException(
+                    "You must specify the filePath property of " +
+                            learn.prospring5.ch04.hooking.into.bean.destruction.UsingtheJSR.DestructiveBeanWithJSR250.class);
+        }
+        this.file = new File(filePath);
+        this.file.createNewFile();
+        System.out.println("File exists: " +  file.exists());
+    }
+
+    public void destroy() {
+        System.out.println("Destroying  Bean");
+        if(!file.delete()) {
+            System.err.println("ERROR: failed  to delete file.");
+        }
+
+
+        System.out.println("File exists: " +  file.exists());
+    }
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
