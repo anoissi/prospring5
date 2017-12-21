22afaf61ba249e4f47f938d3ad7f993b83eb6c8d
anoissi <abdessattar.noi@gmail.com>
2017-12-21 16:40:49 +0100

    use of factory beans

---
+++ chapter04/src/main/java/learn/prospring5/ch04/use/of/factorybeans/MessageDigestDemo.java
@@ -0,0 +1,15 @@
+package learn.prospring5.ch04.use.of.factorybeans;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+public class MessageDigestDemo {
+    public static void main(String... args) {
+        GenericXmlApplicationContext  ctx  =
+                new GenericXmlApplicationContext();
+        ctx.load("classpath:useOfFactoryBeans/fact-beans.xml");
+        ctx.refresh();
+        MessageDigester digester = ctx.getBean("digester",
+                MessageDigester.class);
+        digester.digest("Hello World!");
+        ctx.close();
+    }
+}
