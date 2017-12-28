396adb68bf26690f203cc1b456310f94a2819c36
anoissi <abdessattar.noi@gmail.com>
2017-12-28 10:37:41 +0100

    add a stored function sql script

---
+++ sqlqueries/function_getFirstNameById.sql
@@ -0,0 +1,10 @@
+DROP FUNCTION getFirstNameById();
+CREATE OR REPLACE FUNCTION getFirstNameById(in_id INT)
+RETURNS VARCHAR(60) AS $firstName$
+declare
+	firstName VARCHAR(60);
+BEGIN
+  SELECT first_name into firstName FROM singer WHERE id = in_id;
+  RETURN  firstName;
+END;
+$firstName$ LANGUAGE plpgsql;
