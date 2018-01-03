ed4c65b88f69cc4f83ebca94f955d4e718c1dc0e
anoissi <abdessattar.noi@gmail.com>
2018-01-03 16:24:12 +0100

    Querying with Untyped Results

---
+++ chapter08/src/test/java/learn/prospring5/ch08/SingerSummaryJPATest.java
@@ -0,0 +1,37 @@
+package learn.prospring5.ch08;
+
+import learn.prospring5.ch08.services.SingerSummaryUntypeImpl;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static junit.framework.TestCase.assertNotNull;
+
+public class SingerSummaryJPATest {
+    private static Logger logger =
+            LoggerFactory.getLogger(SingerSummaryJPATest.class);
+    private GenericApplicationContext ctx;
+    private SingerSummaryUntypeImpl singerSummaryUntype;
+
+    @Before
+    public void setUp() {
+        ctx = new GenericXmlApplicationContext("app-context-annotation.xml");
+        singerSummaryUntype = ctx.getBean(SingerSummaryUntypeImpl.class);
+        assertNotNull(singerSummaryUntype);
+    }
+
+    @Test
+    public void testFindAllUntype() {
+        singerSummaryUntype.displayAllSingerSummary();
+    }
+
+    @After
+    public void tearDown() {
+        ctx.close();
+    }
+}
