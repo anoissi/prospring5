59d2f8d13d2374fe27b9f8f0992fbe5b4e5b3097
anoissi <abdessattar.noi@gmail.com>
2018-01-09 14:57:06 +0100

    spring mvc paginiation

--- chapter16/src/main/java/learn/prospring5/ch16/web/SingerController.java
+++ chapter16/src/main/java/learn/prospring5/ch16/web/SingerController.java
@@ -1,20 +1,23 @@
 package learn.prospring5.ch16.web;
 
 
+import com.google.common.collect.Lists;
 import learn.prospring5.ch16.entities.Singer;
 import learn.prospring5.ch16.services.SingerService;
 import learn.prospring5.ch16.utils.Message;
+import learn.prospring5.ch16.utils.SingerGrid;
 import learn.prospring5.ch16.utils.UrlUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
@@ -100,6 +103,46 @@ public class SingerController {
         uiModel.addAttribute("singer", singer);
         return "singers/create";
     }
+
+
+    @ResponseBody
+    @RequestMapping(value =  "/listgrid", method  =  RequestMethod.GET, produces="application/json")
+    public SingerGrid listGrid(@RequestParam(value =  "page", required =  false)  Integer page,
+                               @RequestParam(value =  "rows", required =  false)  Integer rows,
+                               @RequestParam(value =  "sidx",  required =  false) String sortBy,
+                               @RequestParam(value =  "sord",  required =  false) String order) {
+        logger.info("Listing  singers for  grid with page: {}, rows: {}", page, rows);
+        logger.info("Listing singers for grid with sort:  {}, order: {}", sortBy, order);
+        // Process order  by
+        Sort sort =  null;
+        String  orderBy =  sortBy;
+        if (orderBy != null  && orderBy.equals("birthDateString"))
+            orderBy =  "birthDate";
+        if (orderBy != null  && order != null)  {
+            if (order.equals("desc")) {
+                sort =  new Sort(Sort.Direction.DESC, orderBy);
+            } else
+                sort =  new Sort(Sort.Direction.ASC, orderBy);
+        }
+        // Constructs page  request for  current  page
+        // Note: page  number  for  Spring Data   JPA starts with 0,
+        // while jqGrid starts  with 1
+        PageRequest pageRequest  =  null;
+        if (sort != null) {
+            pageRequest =  PageRequest.of(page - 1, rows, sort);
+        } else {
+            pageRequest =  PageRequest.of(page - 1, rows);
+        }
+        Page<Singer> singerPage  =  singerService.findAllByPage(pageRequest);
+        // Construct the grid  data that  will  return as  JSON data
+        SingerGrid singerGrid =  new SingerGrid();
+        singerGrid.setCurrentPage(singerPage.getNumber() +  1);
+        singerGrid.setTotalPages(singerPage.getTotalPages());
+        singerGrid.setTotalRecords(singerPage.getTotalElements());
+        singerGrid.setSingerData(Lists.newArrayList(singerPage.iterator()));
+        return singerGrid;
+    }
+
 
     @Autowired
     public void setSingerService(SingerService singerService) {
